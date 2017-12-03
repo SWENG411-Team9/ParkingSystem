7b8093623d512b0a92958a2f38f3ecf5c87ccb66
ict5012 <32579793+ict5012@users.noreply.github.com>
2017-12-03 15:19:31 -0500

    Update Users.sql

--- Users.sql
+++ Users.sql
@@ -3,6 +3,6 @@
 	[employeeID] NUMERIC(9) NOT NULL PRIMARY KEY, 
     [username] NCHAR(10) NOT NULL, 
     [password] NCHAR(20) NOT NULL, 
-    [type] INT NOT NULL, 
+    [uType] INT NOT NULL, 
     [badgeNum] NUMERIC(10) NULL
 )
