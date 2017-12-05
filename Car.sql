2965c87ccca2e8b33eb95e02536f83e969fea864
nkkoufo <nkkoufo@gmail.com>
2017-12-04 19:18:21 -0500

    im sorry

--- Car.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Car] (
-    [plateNum] NCHAR (7) NOT NULL,
-    [make]     NCHAR (15)  NOT NULL,
-    [model]    NCHAR (15)  NOT NULL,
-    [color]    NCHAR (15)  NOT NULL,
-    PRIMARY KEY CLUSTERED ([plateNum] ASC)
-);
-
--- Parker.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Parker] (
-    [parkerID] NUMERIC (9) NOT NULL,
-    [fname]    NCHAR (15)  NOT NULL,
-    [lname]    NCHAR (15)  NOT NULL,
-    PRIMARY KEY CLUSTERED ([parkerID] ASC)
-);
-
